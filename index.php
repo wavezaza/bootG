b6cb3d71e676925a7cf0e23f0372d43ad9f688e5
ReStarT <80101494+wavezaza@users.noreply.github.com>
2021-12-16 16:40:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="YOU/PATH/dist/css/custom-alert-bootstrap.css"> 
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
-<body onload="Swal()">
+<body  onload="Swal()">
     <!-- nav เปิด -->
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark sticky-top mb-5">
         <div class="container-fluid">
@@ -23,7 +23,7 @@
             <div class="navbar-collapse collapse" id="navbarToggle" >
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">หน้าหลัก</a>
+                        <a href="#foorter" class="nav-link">หน้าหลัก</a>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">About</a>
@@ -137,11 +137,69 @@
             </div>
         </div>
     </div>
+    <div class="card container border-dark border-3  mb-5 py-4 bg-secondary " >
+        <div class="container mb-4">
+            <div class="row">
+                <div class="col-lg-8 m-auto">
+                    <div class="carousel slide" id="slider1" data-bs-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <button data-bs-target="#slider1" data-bs-slide-to="0" class="active" aria-current="true"></button>
+                                <button data-bs-target="#slider1" data-bs-slide-to="1" class=""></button>
+                                <button class="" data-bs-target="#slider1" data-bs-slide-to="2"></button>
+                                <button data-bs-target="#slider1" data-bs-slide-to="3" class=""></button>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item carousel-item-next carousel-item-start">
+                                    <img src="https://cdn.pixabay.com/photo/2021/02/08/15/02/mountains-5995081__340.jpg" class="d-block w-100">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h3>ภูเขา</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, nobis.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://cdn.pixabay.com/photo/2020/06/15/01/06/sunset-5299957__340.jpg" class="d-block w-100">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h3>ทะเล</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, nobis.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="https://cdn.pixabay.com/photo/2021/01/18/17/46/sunset-5928907__340.jpg" class="d-block w-100">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h3>พระอาทิตย์ตกดิน</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, nobis.</p>
+                                    </div>
+                                </div>
+                                <div class="carousel-item active carousel-item-start">
+                                    <img src="https://cdn.pixabay.com/photo/2021/01/21/14/10/egret-5937499__340.jpg" class="d-block w-100">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h3>หมอก</h3>
+                                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, nobis.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" data-bs-slide="prev" data-bs-target="#slider1">
+                                <span class="carousel-control-prev-icon"></span>
+                            </button>
+                            <button class="carousel-control-next" data-bs-slide="next" data-bs-target="#slider1">
+                                <span class="carousel-control-next-icon"></span>
+                            </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card  border-dark border-2 bg-secondary text-white">
+            <div class="card-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto necessitatibus libero veniam doloremque soluta. Commodi, similique, nesciunt corporis laboriosam fuga sapiente non blanditiis inventore quasi labore tempora enim dicta a!</p>
+            </div>
+        </div>
+    </div>
     <!-- context ปิด -->
 
     <!-- foorter เปิด -->
     <footer class="bg-dark text-center text-white">
         <div class="container p-4 pb-0">
+            <a name="foorter"></a>
             <section class="mb-4">
                 <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
                         class="fab fa-facebook-f"></i></a>
@@ -168,6 +226,9 @@
         </div>
     </footer>
     <!-- foorter ปิด -->
+    <div class="container-fluid text-end  fixed-bottom">
+        <button style="width: 50px; height: 50px;" class="btn-dark" onclick="jump('top')">^</button>
+    </div>
 </body>
 <script>
     let timerInterval
@@ -175,7 +236,7 @@
         title: 'ยินดีต้อนรับเข้าสู่เว็บไซต์',
         html: 'หน้าต่างจะปิดใน <b></b> ms.',
         backdrop: 'rgba(0, 0, 0, 0.9)',
-        timer: 2700,
+        timer: 2000,
         timerProgressBar: true,
         didOpen: () => {
             Swal.showLoading()
@@ -194,5 +255,11 @@
         }
     })
     
+
+    function jump(h){
+        let url = location.href;
+        location.href = "#"+h; 
+        history.replaceState(null,null,url);
+    }
 </script>
 </html>
